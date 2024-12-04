3b637c6cd539721bdd69fcc05d8b314897640cc5
inaijin <kouroshsajjadi@yahoo.com>
2024-12-05 01:15:19 +0330

    fixed Bug Link Not Found AS Update

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.cpp
@@ -5,6 +5,17 @@ Node::Node(int id, const QString &ipAddress, QObject *parent)
 {
 }
 
+int Node::getId() const
+{
+    return m_id;
+}
+
+QString Node::getIPAddress() const
+{
+    return m_ipAddress;
+}
+
+
 Node::~Node()
 {
     // Virtual destructor implementation
