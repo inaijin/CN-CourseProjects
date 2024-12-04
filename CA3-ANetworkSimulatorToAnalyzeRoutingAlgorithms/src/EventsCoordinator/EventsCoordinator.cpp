264b834598485e87a0cfc2ac26cfbc224d2c9803
inaijin <kouroshsajjadi@yahoo.com>
2024-12-04 16:00:55 +0330

    add Init EventsCoordinator Header + CPP

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.cpp
@@ -1,26 +1,57 @@
-// #include "EventsCoordinator.h"
+#include "EventsCoordinator.h"
+#include <QDebug>
 
-// EventsCoordinator::EventsCoordinator(QThread *parent) :
-//     QThread {parent}
-// {}
+EventsCoordinator::EventsCoordinator(QThread *parent) :
+    QThread {parent},
+    m_timer {new QTimer(this)}
+{
+    connect(m_timer, &QTimer::timeout, this, &EventsCoordinator::onTick);
+}
 
-// EventsCoordinator *
-// EventsCoordinator::instance(QThread *parent)
-// {
-//     if(!m_self)
-//     {
-//         m_self = new EventsCoordinator(parent);
-//     }
+EventsCoordinator *
+EventsCoordinator::instance(QThread *parent)
+{
+    if (!m_self)
+    {
+        m_self = new EventsCoordinator(parent);
+    }
 
-//     return m_self;
-// }
+    return m_self;
+}
 
-// void
-// EventsCoordinator::release()
-// {
-//     if(m_self)
-//     {
-//         delete m_self;
-//         m_self = nullptr;
-//     }
-// }
+void
+EventsCoordinator::release()
+{
+    if (m_self)
+    {
+        delete m_self;
+        m_self = nullptr;
+    }
+}
+
+void
+EventsCoordinator::startClock(Millis interval)
+{
+    if (!m_timer->isActive())
+    {
+        m_timer->start(interval.count());
+        qDebug() << "Clock started with interval:" << interval.count() << "ms";
+    }
+}
+
+void
+EventsCoordinator::stopClock()
+{
+    if (m_timer->isActive())
+    {
+        m_timer->stop();
+        qDebug() << "Clock stopped.";
+    }
+}
+
+void
+EventsCoordinator::onTick()
+{
+    emit tick();
+    qDebug() << "Tick emitted.";
+}
--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.h
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.h
@@ -1,36 +1,37 @@
-// #ifndef EVENTSCOORDINATOR_H
-// #define EVENTSCOORDINATOR_H
+#ifndef EVENTSCOORDINATOR_H
+#define EVENTSCOORDINATOR_H
 
-// #include <chrono>
+#include <chrono>
+#include <QObject>
+#include <QThread>
+#include <QTimer>
 
-// #include <QObject>
-// #include <QThread>
+class EventsCoordinator : public QThread
+{
+    Q_OBJECT
 
-// class EventsCoordinator : public QThread
-// {
-//     Q_OBJECT
+    typedef std::chrono::milliseconds Millis;
 
-//     typedef std::chrono::milliseconds Millis;
+    explicit EventsCoordinator(QThread *parent = nullptr);
 
-//     explicit EventsCoordinator(QThread *parent = nullptr);
+public:
+    ~EventsCoordinator() override = default;
+    static EventsCoordinator *instance(QThread *parent = nullptr);
+    static void               release();
 
-// public:    // constructors
-//     ~EventsCoordinator() override = default;
-//     static EventsCoordinator *instance(QThread *parent = nullptr);
-//     static void               release();
+public:
+    void startClock(Millis interval);
+    void stopClock();
 
-// public:    // methods
+Q_SIGNALS:
+    void tick();
 
-// public:    // getter and setters
-// Q_SIGNALS:
+private Q_SLOTS:
+    void onTick();
 
+private:
+    inline static EventsCoordinator *m_self = nullptr;
+    QTimer *m_timer = nullptr;
+};
 
-// public Q_SLOTS:
-
-// private Q_SLOTS:
-
-// private:    // members
-//     inline static EventsCoordinator *m_self = nullptr;
-// };
-
-// #endif    // EVENTSCOORDINATOR_H
+#endif
