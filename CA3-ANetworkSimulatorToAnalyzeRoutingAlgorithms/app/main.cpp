68933c6410eb0657adb65ed61ddae3beb0167d14
inaijin <kouroshsajjadi@yahoo.com>
2024-12-04 20:32:49 +0330

    update Main To Test ISSUE SharedPointer Bug

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/app/main.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/app/main.cpp
@@ -1,13 +1,20 @@
 #include <QCoreApplication>
-#include <iostream>
+#include "../src/NetworkSimulator/Simulator.h"
 
-int
-main(int argv, char* argc[])
+int main(int argv, char* argc[])
 {
 
     QCoreApplication app(argv, argc);
 
-    std::cout << "TEST" << std::endl;
+    Simulator simulator;
+    if (!simulator.loadConfig("config.json"))
+    {
+        qCritical() << "Failed to load configuration.";
+        return -1;
+    }
+
+    simulator.initializeNetwork();
+    simulator.startSimulation();
 
     return app.exec();
 }
