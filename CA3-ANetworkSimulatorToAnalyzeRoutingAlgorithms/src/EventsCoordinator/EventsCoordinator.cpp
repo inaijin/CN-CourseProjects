aad34c54ee58e86e159bc5d9685f9827bd6dea82
inaijin <kouroshsajjadi@yahoo.com>
2024-12-04 17:14:04 +0330

    add Link Between EventsCoordinator And DataGenerator

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.cpp
@@ -1,9 +1,11 @@
 #include "EventsCoordinator.h"
+#include "../DataGenerator/DataGenerator.h"
 #include <QDebug>
 
 EventsCoordinator::EventsCoordinator(QThread *parent) :
     QThread {parent},
-    m_timer {new QTimer(this)}
+    m_timer {new QTimer(this)},
+    m_dataGenerator {nullptr}  // Initialize DataGenerator pointer
 {
     connect(m_timer, &QTimer::timeout, this, &EventsCoordinator::onTick);
 }
@@ -50,8 +52,37 @@ EventsCoordinator::stopClock()
 }
 
 void
+EventsCoordinator::setDataGenerator(DataGenerator *generator)
+{
+    if (m_dataGenerator)
+    {
+        disconnect(m_dataGenerator, &DataGenerator::packetsGenerated, this, &EventsCoordinator::onPacketsGenerated);
+    }
+
+    m_dataGenerator = generator;
+
+    if (m_dataGenerator)
+    {
+        connect(m_dataGenerator, &DataGenerator::packetsGenerated, this, &EventsCoordinator::onPacketsGenerated);
+    }
+}
+
+void
 EventsCoordinator::onTick()
 {
     emit tick();
     qDebug() << "Tick emitted.";
+
+    if (m_dataGenerator)
+    {
+        m_dataGenerator->generatePackets();
+        qDebug() << "DataGenerator triggered to generate packets.";
+    }
+}
+
+void
+EventsCoordinator::onPacketsGenerated(const std::vector<QSharedPointer<Packet>> &packets)
+{
+    emit dataGenerated(packets);
+    qDebug() << "Packets generated and forwarded. Count:" << packets.size();
 }
--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.h
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/EventsCoordinator/EventsCoordinator.h
@@ -5,6 +5,11 @@
 #include <QObject>
 #include <QThread>
 #include <QTimer>
+#include <QSharedPointer>
+#include <vector>
+
+class DataGenerator;
+class Packet;
 
 class EventsCoordinator : public QThread
 {
@@ -19,19 +24,23 @@ public:
     static EventsCoordinator *instance(QThread *parent = nullptr);
     static void               release();
 
-public:
     void startClock(Millis interval);
     void stopClock();
 
+    void setDataGenerator(DataGenerator *generator);
+
 Q_SIGNALS:
     void tick();
+    void dataGenerated(const std::vector<QSharedPointer<Packet>> &packets);
 
 private Q_SLOTS:
     void onTick();
+    void onPacketsGenerated(const std::vector<QSharedPointer<Packet>> &packets);
 
 private:
     inline static EventsCoordinator *m_self = nullptr;
     QTimer *m_timer = nullptr;
+    DataGenerator *m_dataGenerator = nullptr;
 };
 
 #endif
