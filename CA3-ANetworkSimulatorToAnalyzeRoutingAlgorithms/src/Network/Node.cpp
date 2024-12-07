b8b0e6ea60547786e4294d0be0dba808bb47dcbe
inaijin <kouroshsajjadi@yahoo.com>
2024-12-07 20:13:45 +0330

    update Node CPP + Header For Better Allignment Phase 2

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.cpp
@@ -1,20 +1,27 @@
 #include "Node.h"
 
-Node::Node(int id, const QString &ipAddress, QObject *parent)
-    : QThread(parent), m_id(id), m_ipAddress(ipAddress)
+Node::Node(int id, const QString &ipAddress, NodeType type, QObject *parent)
+    : QThread(parent), m_id(id), m_ipAddress(ipAddress), m_type(type)
 {
 }
 
 int Node::getId() const
 {
+    QMutexLocker locker(&m_mutex);
     return m_id;
 }
 
 QString Node::getIPAddress() const
 {
+    QMutexLocker locker(&m_mutex);
     return m_ipAddress;
 }
 
+NodeType Node::getNodeType() const
+{
+    QMutexLocker locker(&m_mutex);
+    return m_type;
+}
 
 Node::~Node()
 {
--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.h
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/Network/Node.h
@@ -4,23 +4,34 @@
 #include <QThread>
 #include <QObject>
 #include <QString>
+#include <QMutex>
+#include "../Packet/Packet.h"
+
+enum class NodeType
+{
+    PC,
+    Router
+};
 
 class Node : public QThread
 {
     Q_OBJECT
 
 public:
-    explicit Node(int id, const QString &ipAddress, QObject *parent = nullptr);
+    explicit Node(int id, const QString &ipAddress, NodeType type, QObject *parent = nullptr);
     virtual ~Node() = 0;
 
     int getId() const;
     QString getIPAddress() const;
+    NodeType getNodeType() const;
 
 protected:
     virtual void run() override = 0;
 
     int m_id;
     QString m_ipAddress;
+    NodeType m_type;
+    mutable QMutex m_mutex;
 };
 
 #endif // NODE_H
