2ce65042efbed768464d74bb9b8ea0c02c3fdca6
inaijin <kouroshsajjadi@yahoo.com>
2024-12-12 13:04:26 +0330

    add DHCPServer CPP + Header

--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/DHCPServer/DHCPServer.cpp
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/DHCPServer/DHCPServer.cpp
@@ -1,9 +1,65 @@
-// #include "DHCPServer.h"
+#include "DHCPServer.h"
+#include <QDebug>
 
-// DHCPServer::DHCPServer(QObject *parent) :
-//     QObject {parent}
-// {
-//     connect(this, &DHCPServer::sendOffer, this, &DHCPServer::sendPacket);
-// }
+DHCPServer::DHCPServer(int asId, Port *port, QObject *parent)
+    : QObject(parent),
+    m_asId(asId),
+    m_port(port),
+    m_nextAvailableId(1),
+    m_currentTime(0)
+{
+    if (m_asId == 1) {
+        m_ipPrefix = "192.168.100.";
+    } else if (m_asId == 2) {
+        m_ipPrefix = "192.168.200.";
+    } else {
+        qWarning() << "Unsupported AS ID:" << m_asId;
+    }
 
-// DHCPServer::~DHCPServer() {}
+    connect(m_port, &Port::packetReceived, this, &DHCPServer::receivePacket);
+}
+
+DHCPServer::~DHCPServer() {}
+
+void DHCPServer::receivePacket(const PacketPtr_t &packet) {
+    if (!packet || packet->getType() != PacketType::Control) {
+        return;
+    }
+
+    QString payload = packet->getPayload();
+    if (payload.contains("DHCP_REQUEST")) {
+        assignIP(packet);
+    }
+}
+
+void DHCPServer::assignIP(const PacketPtr_t &packet) {
+    QString ipAddress = m_ipPrefix + QString::number(m_nextAvailableId);
+    int clientId = packet->getPayload().split(":")[1].toInt();
+
+    DHCPLease lease = {ipAddress, clientId, m_currentTime + LEASE_DURATION};
+    m_leases.append(lease);
+    m_nextAvailableId++;
+
+    sendOffer(lease);
+}
+
+void DHCPServer::sendOffer(const DHCPLease &lease) {
+    QString payload = QString("DHCP_OFFER:%1:%2").arg(lease.ipAddress).arg(lease.clientId);
+    auto offerPacket = QSharedPointer<Packet>::create(PacketType::Control, payload);
+    emit broadcastPacket(offerPacket);
+    qDebug() << "Sent DHCP offer:" << payload;
+}
+
+void DHCPServer::tick(int currentTime) {
+    m_currentTime = currentTime;
+    reclaimExpiredLeases();
+}
+
+void DHCPServer::reclaimExpiredLeases() {
+    for (int i = m_leases.size() - 1; i >= 0; --i) {
+        if (m_leases[i].leaseExpirationTime <= m_currentTime) {
+            qDebug() << "Reclaiming expired IP:" << m_leases[i].ipAddress;
+            m_leases.removeAt(i);
+        }
+    }
+}
--- CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/DHCPServer/DHCPServer.h
+++ CA3-ANetworkSimulatorToAnalyzeRoutingAlgorithms/src/DHCPServer/DHCPServer.h
@@ -1,24 +1,45 @@
-// #ifndef DHCPSERVER_H
-// #define DHCPSERVER_H
+#ifndef DHCPSERVER_H
+#define DHCPSERVER_H
 
-// #include <QObject>
+#include <QObject>
+#include <QVector>
+#include <QSharedPointer>
+#include "../Packet/Packet.h"
+#include "../Port/Port.h"
 
-// class DHCPServer : public QObject
-// {
-//     Q_OBJECT
+class DHCPServer : public QObject
+{
+    Q_OBJECT
 
-// public:    // constructors
-//     explicit DHCPServer(QObject *parent = nullptr);
-//     ~DHCPServer() override;
+public:
+    explicit DHCPServer(int asId, Port *port, QObject *parent = nullptr);
+    ~DHCPServer() override;
 
-// public:    // methods
-// Q_SIGNALS:
+    void receivePacket(const PacketPtr_t &packet);
+    void tick(int currentTime);
 
-// public Q_SLOTS:
+Q_SIGNALS:
+    void broadcastPacket(const PacketPtr_t &packet);
 
-// private:    // methods
+private:
+    struct DHCPLease {
+        QString ipAddress;
+        int clientId;
+        int leaseExpirationTime;
+    };
 
-// private:    // members
-// };
+    void assignIP(const PacketPtr_t &packet);
+    void sendOffer(const DHCPLease &lease);
+    void reclaimExpiredLeases();
 
-// #endif      // DHCPSERVER_H
+    int m_asId;
+    Port *m_port;
+    QVector<DHCPLease> m_leases;
+    QString m_ipPrefix;
+    int m_nextAvailableId;
+    int m_currentTime;
+
+    static const int LEASE_DURATION = 300; // Lease duration in seconds
+};
+
+#endif // DHCPSERVER_H
